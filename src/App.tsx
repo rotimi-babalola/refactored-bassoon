c9aac16e458b3c0792e29bd185fc71b851d792ad
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-16 17:48:30 +0200

    Add lint github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
+console.log('kfkfk');
+
 const App: React.FC<{}> = () => (
   <Typography variant="h3">Upload to S3</Typography>
 );
