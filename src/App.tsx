5b16fb5ef4970080f342689c3ee4b4b49eb071ac
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-16 17:52:20 +0200

    Remove console statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
-console.log('kfkfk');
-
 const App: React.FC<{}> = () => (
   <Typography variant="h3">Upload to S3</Typography>
 );
