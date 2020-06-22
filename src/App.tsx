a568f309229bb57276998286adda4e4edd95eb3b
Rotimi Babalola <tolurotimi@hotmail.com>
2020-06-22 13:18:02 +0200

    Add initial code for uploading to S3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { SnackbarProvider } from 'notistack';
 import Upload from './components/Upload';
 
 const App: React.FC<{}> = () => (
   <div style={{ margin: '0 auto', width: '50%' }}>
-    <Typography variant="h3">Upload to S3</Typography>
-    <Upload />
+    <SnackbarProvider
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      maxSnack={3}
+    >
+      <Typography variant="h3">Upload to S3</Typography>
+      <Upload />
+    </SnackbarProvider>
   </div>
 );
 
--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,21 +1,78 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useRef, useState } from 'react';
+import {
+  Button,
+  Typography,
+  createStyles,
+  makeStyles,
+} from '@material-ui/core';
+import useUploadFile from '../hooks/useUploadFile';
 
-const Upload = () => (
-  <div style={{ marginTop: '20px' }}>
-    <label htmlFor="contained-button-file">
-      <input
-        type="file"
-        accept="image/*,.pdf"
-        id="contained-button-file"
-        style={{ display: 'none' }}
-        multiple
-      />
-      <Button variant="contained" color="primary" component="span">
-        Upload
-      </Button>
-    </label>
-  </div>
+const useStyles = makeStyles(() =>
+  createStyles({
+    buttonContainer: {
+      marginBottom: '40px',
+    },
+  }),
 );
+
+const Upload = () => {
+  const classes = useStyles();
+  const inputRef = useRef(null);
+  const [files, setFiles] = useState<File[]>();
+
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    // array of object to stored dropped files
+    const droppedFiles = [];
+    for (let i = 0; i < evt.target.files.length; i++) {
+      droppedFiles.push(evt.target.files.item(i));
+    }
+
+    setFiles(droppedFiles);
+  };
+
+  const handleUpload = () => {
+    useUploadFile(files);
+  };
+
+  return (
+    <div style={{ marginTop: '20px' }}>
+      <div className={classes.buttonContainer}>
+        <label htmlFor="contained-button-file">
+          <input
+            type="file"
+            accept="image/*,.pdf"
+            id="contained-button-file"
+            style={{ display: 'none' }}
+            multiple
+            ref={inputRef}
+            onChange={handleChange}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            component="span"
+            style={{ marginRight: '20px' }}
+          >
+            Select Files
+          </Button>
+        </label>
+        <Button
+          variant="contained"
+          color="primary"
+          component="span"
+          onClick={handleUpload}
+          disabled={!files}
+        >
+          Upload to S3
+        </Button>
+      </div>
+      {files ? (
+        files.map(file => <Typography key={file.name}>{file.name}</Typography>)
+      ) : (
+        <Typography>No files</Typography>
+      )}
+    </div>
+  );
+};
 
 export default Upload;
